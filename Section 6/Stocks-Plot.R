0c4ad2d70c4a8d2f0dc882f8f13c39cfdc96523a
MarWaltz <67819196+MarWaltz@users.noreply.github.com>
2020-07-09 13:34:55 +0200

    Update Stocks-Plot.R

--- Section 6/Stocks-Plot.R
+++ Section 6/Stocks-Plot.R
@@ -1,3 +1,6 @@
+#Note: The calculations in this script take only a couple of seconds on an Intel(R) Xeon(R)
+#      Gold 6136 CPU with 3.00 GHz on a 64-Bit Windows Server 2016 with 24 processors.
+
 library("quantmod")
 library("lubridate")
 
